5576a591ace670b7cf5cced3cc102bad2b407722
Бовыка Ольга <olga@skat-vending.com>
2020-05-13 19:45:39 +0300

    1. Поработайте с переменными...

--- task1.py
+++ task1.py
@@ -7,6 +7,6 @@ if var_input.isdigit() and var_input2.isdigit():
     my_var_int = int(var_input)
     my_var_int2 = int(var_input2)
     my_var_int3 = my_var_int + my_var_int2
-    print(var_input3,", сумма целочисленных переменных ", my_var_int3)
+    print(var_input3, ", сумма целочисленных переменных ", my_var_int3)
 else:
-    print(var_input3,", мы ждем ввода целых чисел")
+    print(var_input3, ", мы ждем ввода целых чисел")
