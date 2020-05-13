c68f6e91bc5e0cf4d98ce02f87bc3b7991daec16
Бовыка Ольга <olga@skat-vending.com>
2020-05-13 14:33:11 +0300

    1. Поработайте с переменными...

---
+++ task1.py
@@ -0,0 +1,12 @@
+my_var_int = 0
+my_var_int2 = 0
+var_input = input("Введите целочисленную переменную: ")
+var_input2 = input("Введите целочисленную переменную: ")
+var_input3 = input("Введите свое имя ")
+if var_input.isdigit() and var_input2.isdigit():
+    my_var_int = int(var_input)
+    my_var_int2 = int(var_input2)
+    my_var_int3 = my_var_int + my_var_int2
+    print(var_input3,", сумма целочисленных переменных ", my_var_int3)
+else:
+    print(var_input3,", мы ждем ввода целых чисел")
